5391cc456edea9cdb0d1d62d454d9a6140f55612
Pedro Borges <pedroborgespj@gmail.com>
2025-07-14 09:19:37 -0300

    removendo o timeout do cenário em home.spec

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -6,5 +6,4 @@ test('deve exibir o titulo', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Linkaí/);
 
-  await page.waitForTimeout(3000)
 });
