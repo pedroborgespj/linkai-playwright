246b218d83c2cfc4c0a992244f164a316e848ec1
Pedro Borges <pedroborgespj@gmail.com>
2025-07-17 09:05:07 -0300

    atualizando o codigo gerado pelo codegen do pw

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -3,18 +3,25 @@ import {test, expect} from '@playwright/test'
 test('deve logar com sucesso', async ({page})=> {
 
     const user = {
+        name: 'Beth',
         username: 'beth',
         password: 'pwd123'
     }
 
     await page.goto('http://localhost:3000/login')
 
-    await page.locator('input[placeholder="Seu @username incrível"]').fill(user.username)
-    await page.locator('input[placeholder="Digite sua senha secreta"]').fill(user.password)
+    await page
+        .getByRole('textbox', { name: 'Seu @username incrível'})
+        .fill(user.username)
+    
+    await page
+        .getByRole('textbox', { name: 'Digite sua senha secreta'})
+        .fill(user.password)
 
-    await page.locator('button[type="submit"]').click()
+    await page
+        .getByRole('button', { name: 'Entrar'})
+        .click()
 
-    const title = page.locator('h1')
-    await expect(title).toContainText('Olá, Beth!')
+    await expect(page.locator('h1')).toContainText(`Olá, ${user.name}!`)
 
 })
